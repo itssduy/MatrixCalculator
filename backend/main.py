f203b91c8c95b5271c95db29c44980d82fb52da2
Duy <duypersonal512@gmail.com>
2026-02-16 15:45:53 -0600

    feat: connect app to db & create matrix CRUD endpoints

---
+++ backend/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg
+from uuid import UUID, uuid4
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_port = os.getenv("DB_PORT")
+
+class Matrix(BaseModel):
+    elements: list[list[int]]
+
+app = FastAPI()
+
+connectionString = f"postgresql://{db_user}:{db_pass}@localhost:{db_port}/{db_name}"
+
+@app.get("/matrices/")
+async def read_matrices():
+    with psycopg.connect(connectionString) as conn: 
+        with conn.cursor() as cur:
+            records = cur.execute("SELECT * FROM matrix").fetchall()
+            return records
+
+@app.get("/matrices/{matrix_id}")
+async def read_matrix(matrix_id):
+    with psycopg.connect(connectionString) as conn: 
+        with conn.cursor() as cur:
+            record = cur.execute("SELECT * FROM matrix WHERE id=%s", (matrix_id)).fetchone()
+            return record
+
+@app.post("/matrix")
+async def create_matrix(matrix: Matrix):
+    matrix_id = uuid4()
+    with psycopg.connect(connectionString) as conn: 
+        with conn.cursor() as cur:
+            record = cur.execute("INSERT INTO matrix (id, elements) VALUES (%s, %s) RETURNING *", (matrix_id, matrix.elements)).fetchone()
+            return record
+
+@app.put("/matrix/{matrix_id}")
+async def update_matrix(matrix_id: str, matrix: Matrix):
+    with psycopg.connect(connectionString) as conn:
+        with conn.cursor() as cur:
+            record = cur.execute("UPDATE matrix SET elements=%s WHERE id=%s RETURNING *", (matrix.elements, matrix_id)).fetchone()
+            return record
