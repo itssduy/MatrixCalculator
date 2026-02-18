a55a417483fb67f28bd987c17d08d202d0238a65
Duy <duypersonal512@gmail.com>
2026-02-17 20:21:30 -0600

    feat: add update matrix

--- frontend/src/componenets/pages/Calculator.jsx
+++ frontend/src/componenets/pages/Calculator.jsx
@@ -38,7 +38,7 @@ const Calculator = ()=>{
             }
         }
 
-         const res = await fetch(`${import.meta.env.VITE_API_URL}/matrix`, {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/matrix`, {
             method: "POST",
             mode: "cors",
             headers: {
@@ -61,11 +61,50 @@ const Calculator = ()=>{
             mode: "cors",
             method: "DELETE"
         })
-
+        
         const data = await res.json()
         reloadMatrices()
 
     }
+
+    const updateMatrix = async (e, matrixId) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+
+        const entries = Array.from(formData.entries());
+        const entiresData = entries[entries.length - 1][0].split("-");
+        const rows = parseInt(entiresData[0]) + 1
+        const cols = parseInt(entiresData[1]) + 1
+
+        let matrix = [];
+
+        for (let i = 0; i < rows; i++) {
+            matrix.push([]);
+
+            for (let j = 0; j < cols; j++) {
+                const value = formData.get(`${i}-${j}`);
+                matrix[i].push(Number(value));
+            }
+        }
+
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/matrix/${matrixId}`, {
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                elements: matrix
+            }) 
+        })
+
+        const data = await res.json()
+        console.log(data)
+        reloadMatrices()
+
+    }
+
     return (
         <>
             <h2>Add Matrix</h2>
@@ -78,20 +117,23 @@ const Calculator = ()=>{
             </form>
 
             {getMatrices.map((matrix)=>(
-                <Fragment key={matrix[0]}>
-                <table key={matrix[0]}>
-                <tbody key={`${matrix[0]}-body`}>
-                    {matrix[1].map((row, i)=>(
-                        <tr key={`${matrix[0]}-${i}`}>
-                            {row.map((col, j)=>(
-                                <td key={`${matrix[0]}-${i}-${j}`}>{col}</td>
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-                </table>
-                <button onClick={(e)=>{deleteMatrix(e,matrix[0])}} key={`${matrix[0]}-btn`}>X</button>
-                </ Fragment>
+                <form onSubmit={(e)=>{updateMatrix(e,matrix[0])}} key={`${matrix[0]}-f`}>
+                    <table key={matrix[0]}>
+                    <tbody key={`${matrix[0]}-body`}>
+                        {matrix[1].map((row, i)=>(
+                            <tr key={`${matrix[0]}-${i}`}>
+                                {row.map((col, j)=>(
+                                    <td key={`${matrix[0]}-${i}-${j}`}>
+                                        <input type="number" name={`${i}-${j}`} defaultValue={col}/>
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                    <button type="submit">Edit</button>
+                    <button type="button" onClick={(e)=>{deleteMatrix(e,matrix[0])}} key={`${matrix[0]}-btn`}>X</button>
+                </form>
             ))}    
         </>
     )
